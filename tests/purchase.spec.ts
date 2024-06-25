782795553693b1bfbd1f36a673ea3b9d9b9ce170
barboravranova <79592671+barboravranova@users.noreply.github.com>
2024-06-26 01:55:53 +0200

    created new purchase test and moved login part to beforeEach

--- tests/purchase.spec.ts
+++ tests/purchase.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('login test', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
     await page.goto("https://www.saucedemo.com/");  
     await expect(page.locator(".login_logo")).toHaveText("Swag Labs"); 
     await page.locator("#user-name").fill("standard_user");
@@ -8,4 +8,10 @@ test('login test', async ({ page }) => {
     await expect(page.locator("#login-button")).toHaveText("Login"); 
     await page.locator("#login-button").click();
     await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+  });
+
+test('purchase test', async ({ page }) => {
+    await expect (page.getByRole('img', { name: 'Sauce Labs Fleece Jacket' })).toBeVisible();
+    const imageSrc = await page.getByRole('img', { name: 'Sauce Labs Fleece Jacket' }).getAttribute('src');
+    expect(imageSrc).toContain('/static/media/sauce-pullover-1200x1500.51d7ffaf.jpg');
 });
