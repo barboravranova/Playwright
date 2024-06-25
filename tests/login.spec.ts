7bd630b4371c87dd00d5fe4350c207184f9557a3
barboravranova <79592671+barboravranova@users.noreply.github.com>
2024-06-26 00:11:11 +0200

    Add checks for logo and button text

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('login test', async ({ page }) => {
-    await page.goto("https://www.saucedemo.com/");   
+    await page.goto("https://www.saucedemo.com/");  
+    await expect(page.locator(".login_logo")).toHaveText("Swag Labs"); 
     await page.locator("#user-name").fill("standard_user");
     await page.locator("#password").fill("secret_sauce");
+    await expect(page.locator("#login-button")).toHaveText("Login"); 
     await page.locator("#login-button").click();
     await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
 });
