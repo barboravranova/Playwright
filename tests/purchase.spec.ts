2e5af735469d77edb59f1aaf52a2b0a49d3d02b7
barboravranova <79592671+barboravranova@users.noreply.github.com>
2024-06-26 22:54:41 +0200

    locators refactoring

--- tests/purchase.spec.ts
+++ tests/purchase.spec.ts
@@ -1,13 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-
 test.beforeEach(async ({ page }) => {
     await page.goto("https://www.saucedemo.com/");
     await expect(page.locator(".login_logo")).toHaveText("Swag Labs");
-    await page.locator("#user-name").fill("standard_user");
-    await page.locator("#password").fill("secret_sauce");
-    await expect(page.locator("#login-button")).toHaveText("Login");
-    await page.locator("#login-button").click();
+
+    await page.getByPlaceholder('Username').fill('Jan');
+    await page.getByPlaceholder('Password').fill('Marek');
+
+    await page.getByRole('button', { name: 'Login' }).click();
+
     await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
 });
 
