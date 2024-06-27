afba282ef80b787c43b56dc6cd98169dd38e3a70
barboravranova <79592671+barboravranova@users.noreply.github.com>
2024-06-27 23:09:21 +0200

    Updated user list

--- tests/purchase.spec.ts
+++ tests/purchase.spec.ts
@@ -9,7 +9,7 @@ import { CheckoutCompletePage } from './pages/checkoutCompletePage';
 
 const users = [
     { username: 'standard_user', password: 'secret_sauce' },
-    { username: 'locked_out_user', password: 'secret_sauce' },
+    { username: 'error_user', password: 'secret_sauce' },
     { username: 'problem_user', password: 'secret_sauce' },
     { username: 'performance_glitch_user', password: 'secret_sauce' },
 ];
